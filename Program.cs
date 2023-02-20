8b7f71bb0947d8e780610fbcdd6aadb7cf538c48
Jaspreet Dhillon <d.Jaspreet@gmail.com>
2023-02-19 23:46:48 -0500

    cleanup

--- Program.cs
+++ Program.cs
@@ -6,18 +6,11 @@ using WebApi_EFCore.Data.SeedData;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//IF SQL SERVER
+
 var connectionString = builder.Configuration.GetConnectionString("WebApiDB_SqlServer");
 builder.Services.AddDbContext<EmployeeDbContext>(x => x.UseSqlServer(connectionString));
-//ELSE
-//builder.Services.AddDbContext<EmployeeDbContext>(options =>
-//     options.UseSqlite(builder.Configuration.GetConnectionString("WebApiDB_SqlLite")));
-//builder.Services.AddScoped<IEmployeeRepo, EmployeeRepo>();
-//SeedData.Initialize(new EmployeeDbContext());
 
 var app = builder.Build();
 
@@ -30,25 +23,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-        
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 //app.MapGet("/employees", ([FromServices] IEmployeeRepo empRepo) =>
 //{
